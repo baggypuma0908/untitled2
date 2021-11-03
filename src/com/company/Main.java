2f0c0e5baff54e13912a2604bc754662fc31383d
Manyasha <baggy.puma0908@yandex.by>
2021-11-03 15:13:40 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Task1 task1 = new Task1();
+        System.out.println("int i: " + task1.i);
+        System.out.println("char c: " + task1.c + '\n');
+    }
+}
---
+++ src/com/company/Task1.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Task1 {
+    int i;
+    char c;
+}
