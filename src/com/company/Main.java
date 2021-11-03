f09c7570481f06ff9f0ecd09a45a396cf065dfd8
Manyasha <baggy.puma0908@yandex.by>
2021-11-04 01:14:04 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //Task1
         Task1 task1 = new Task1();
         System.out.println("int i: " + task1.i);
         System.out.println("char c: " + task1.c);
+
+        //Task2
+        System.out.println("Hello world!");
     }
 }
